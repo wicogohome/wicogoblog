69f01bdb17e4dc9860e93019341d34284e2d48e4
Wico Tang <wicotang@gmail.com>
2024-04-25 17:17:03 +0800

    chore: vitepress & vite: update configs #6

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+	lang: "zh-TW",
 	title: "WicogoBlog",
 	description: "A VitePress Site",
+	head: [
+		["link", { rel: "icon", href: "/favicon.ico" }], // example
+		["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }], // example
+	],
+	// sitemap: {
+	// 	hostname: "https://wicotang.com/blog/",
+	// },
+	base: "/blog/",
+	cleanUrls: true,
+	outDir: "../dist/blog/",
+	srcDir: "posts/",
+	lastUpdated: true,
+	markdown: {},
+	vite: {},
 });
--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,7 @@ export default defineConfig({
 	resolve: {
 		alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
 	},
+	build: {
+		outDir: "dist/profolio/",
+	},
 });
