ab53303d12ba42b07a509bb49b2b4b06f69cd462
Wico Tang <wicotang@gmail.com>
2024-07-08 11:12:24 +0800

    feat: Article basic style #10

--- docs/.vitepress/theme/Layout/Article.vue
+++ docs/.vitepress/theme/Layout/Article.vue
@@ -1,15 +1,51 @@
 <script lang="ts">
+import { defineComponent, computed } from "vue";
+import { withBase } from "vitepress";
+import useDataTime from "@@/utils/useDateTime.ts";
+import useFrontmatter from "@@/utils/useFrontmatter.ts";
+
 export default defineComponent({
 	name: "BlogArticle",
 	setup() {
-		return { withBase };
+		const { articleFrontmater } = useFrontmatter();
+		const { toDateString } = useDataTime();
+		const createdAt = computed(() => {
+			if (articleFrontmater.date) {
+				return toDateString(articleFrontmater.date);
+			}
+
+			return "";
+		});
+
+		return { withBase, articleFrontmater, createdAt };
 	},
 });
 </script>
 
 <template>
-	<div>
-		<a :href="withBase('/')">Home</a>
-		<Content class="break-all" />
+	<div class="relative">
+		<div class="flex justify-between">
+			<a :href="withBase('/catagories/' + articleFrontmater.category)"
+				>{{ `< ${articleFrontmater.category} ` }} ></a
+			>
+
+			<div
+				v-if="articleFrontmater.tags"
+				class="text-yellow-dark font-semibold text-right"
+			>
+				<a
+					v-for="(tag, index) in articleFrontmater.tags"
+					:key="index"
+					:href="withBase('/tags/' + tag)"
+				>
+					#{{ tag }}
+				</a>
+			</div>
+		</div>
+
+		<div class="article-title">{{ articleFrontmater.title }}</div>
+		<p class="md:text-4xl text-3xl font-bold text-yellow-dark my-2">| {{ createdAt }}</p>
+		<hr />
+		<Content class="article-content !static p-6" />
 	</div>
 </template>
