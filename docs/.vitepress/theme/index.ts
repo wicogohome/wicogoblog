ed5e9b03d48b82bf5588424bfa3d7845f9db31eb
Wico Tang <wicotang@gmail.com>
2024-07-09 14:50:39 +0800

    feat: add font-awesome support  #10

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,9 +3,15 @@ import Layout from "./Layout/index.vue";
 import type { Theme } from "vitepress";
 import "./styles/main.scss";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 export default {
 	Layout,
-	enhanceApp() {
-		// ...
+	enhanceApp({ app }) {
+		library.add(faGithub, faArrowRight, faArrowLeft);
+		app.component("FontAwesomeIcon", FontAwesomeIcon);
 	},
 } satisfies Theme;
