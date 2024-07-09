0a31c65d1f3e2e82e04fb0f0b3bfe706d301570d
Wico Tang <wicotang@gmail.com>
2024-07-09 11:12:09 +0800

    feat: add mermaid support #10

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "url";
 
 import matter from "gray-matter";
 import eslint from "vite-plugin-eslint";
+import { withMermaid } from "vitepress-plugin-mermaid";
 import useGithubArticles from "./utils/useGithubArticles.ts";
 
 const srcDir: string = "posts/";
@@ -26,42 +27,44 @@ pages.map(({ name, content }) => {
 });
 
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-	lang: "zh-TW",
-	title: "WicoGotBlog",
-	description: "A VitePress Site",
-	head: [
-		["link", { rel: "icon", href: "/favicon.ico" }], // example
-		["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
-		["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
-		[
-			"link",
-			{
-				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Georama:ital,wght@0,100..900;1,100..900&family=Noto+Sans+TC:wght@100..900&family=Zilla+Slab+Highlight:wght@400;700&display=swap",
-			},
+export default defineConfig(
+	withMermaid({
+		lang: "zh-TW",
+		title: "WicoGotBlog",
+		description: "A VitePress Site",
+		head: [
+			["link", { rel: "icon", href: "/favicon.ico" }], // example
+			["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+			["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" }],
+			[
+				"link",
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Georama:ital,wght@0,100..900;1,100..900&family=Noto+Sans+TC:wght@100..900&family=Zilla+Slab+Highlight:wght@400;700&display=swap",
+				},
+			],
 		],
-	],
-	// sitemap: {
-	// 	hostname: "https://wicotang.com/blog/",
-	// },
-	base: "/blog/",
-	outDir: "../dist/blog/",
-	srcDir,
-	lastUpdated: true,
-	markdown: {
-		// shiki
-		theme: "vitesse-dark",
-	},
-	vite: {
-		plugins: [
-			eslint({
-				exclude: ["./node_modules/**", "docs/.vitepress/cache"],
-			}),
-		],
-		resolve: {
-			alias: [{ find: "@@/", replacement: fileURLToPath(new URL("./", import.meta.url)) }],
+		// sitemap: {
+		// 	hostname: "https://wicotang.com/blog/",
+		// },
+		base: "/blog/",
+		outDir: "../dist/blog/",
+		srcDir,
+		lastUpdated: true,
+		markdown: {
+			// shiki
+			theme: "vitesse-dark",
 		},
-	},
-	rewrites,
-});
+		vite: {
+			plugins: [
+				eslint({
+					exclude: ["./node_modules/**", "docs/.vitepress/cache"],
+				}),
+			],
+			resolve: {
+				alias: [{ find: "@@/", replacement: fileURLToPath(new URL("./", import.meta.url)) }],
+			},
+		},
+		rewrites,
+	})
+);
