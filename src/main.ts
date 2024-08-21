73c555368e7a47c10872b5439a5a8f04fe565404
Wico Tang <wicotang@gmail.com>
2024-08-21 17:22:28 +0800

    feat: Image Viewer #14

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import "./assets/styles/main.scss";
 import App from "./App.vue";
 
+import "viewerjs/dist/viewer.css";
+import VueViewer from "v-viewer";
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faMap } from "@fortawesome/free-regular-svg-icons";
@@ -9,4 +12,6 @@ import { faMap as faSolidMap } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faMap, faSolidMap);
 
-createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).mount("#app");
+const app = createApp(App).component("FontAwesomeIcon", FontAwesomeIcon);
+app.use(VueViewer);
+app.mount("#app");
