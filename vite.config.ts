9436c184b31945f217d9b1b9007d6bc35e943e2a
WicoTang <wicotang@gmail.com>
2025-03-14 16:47:57 +0800

    refactor: get settings by repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import eslint from "vite-plugin-eslint";
 import { fileURLToPath, URL } from "url";
 import svgLoader from "vite-svg-loader";
+import fetchSettingsPlugin from "./src/plugins/vite-plugin-fetch-settings";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
 			exclude: ["**/node_modules/**", "./dist/**"],
 		}),
 		svgLoader({ defaultImport: "url" }),
+		fetchSettingsPlugin(),
 	],
 	resolve: {
 		alias: [{ find: "@/", replacement: fileURLToPath(new URL("./src/", import.meta.url)) }],
